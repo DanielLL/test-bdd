170f7573862d846c1f31b75d363db81a8d87da47
DanielLL <daniel.lopez@tangosource.com>
2013-12-02 13:41:43 -0600

    Creating new view for articles and filling out with its corresponding title

--- features/step_definitions/article_steps.rb
+++ features/step_definitions/article_steps.rb
@@ -11,3 +11,24 @@ end
 Then(/^I should see "(.*?)"$/) do |arg1|
   expect(page).to have_content arg1
 end
+
+Given(/^I have no articles$/) do
+  Article.all.count.should == 0
+end
+
+When(/^I click on "(.*?)"$/) do |arg1|
+  click_on arg1
+end
+
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
+  fill_in arg1, with: arg2 
+end
+
+When(/^I press "(.*?)"$/) do |arg1|
+  click_on arg1
+end
+
+Then(/^I should have (\d+) article$/) do |arg1|
+  Article.all.count.should == arg1.to_i
+end
+
