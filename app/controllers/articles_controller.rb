97acf1b8309839e0db8f62790b3fde410620c621
DanielLL <daniel.lopez@tangosource.com>
2013-12-02 13:46:29 -0600

    Added create action in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,17 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+ def create
+    @article = Article.create!(article_attributes)
+    flash[:notice] = "New article created."
+    redirect_to articles_path
+  end
+  private
+
+  def article_attributes
+    params.require(:article).permit(
+      :title, :content
+    )
+  end
 end
