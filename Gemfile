113ae6e774914c6fec7e35bd0e99d7805c3ab7b8
DanielLL <daniel.lopez@tangosource.com>
2013-12-02 13:26:38 -0600

    First Commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.1'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :development, :test do
+  gem "webrat"
+  gem 'database_cleaner'
+  gem 'cucumber-rails', require: false
+  gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'rspec-rails'
+  gem 'pry'
+  gem 'selenium-webdriver'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestBdd::Application.config.secret_key_base = '8dacfee2a8f3534b6951ec004e61bfd98d5172b0e9ee87cde7401873ddfb678e3aab86dad7d8f9d7563865601e9092204b7ba796f30f2b269e637f26446404fb'
