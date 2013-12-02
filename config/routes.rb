acf5cf834f1bfd1a6562eee4060f49597ef2a663
DanielLL <daniel.lopez@tangosource.com>
2013-12-02 13:34:37 -0600

    Added article to routes, articles controller and index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TestBdd::Application.routes.draw do
+  get "articles/index"
+  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
